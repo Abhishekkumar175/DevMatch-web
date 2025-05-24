a1ac01fc48439ceb7f0f7f45eb0ae0c92a27cc38
abhishekkumar <abhishekkumar345ssm@gmail.com>
2025-05-24 17:51:01 +0530

    set-up tailwind&daisy-ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       
-      <h1>Hello World!..</h1>
+     <h1 class="text-3xl font-bold underline text-center text-red-500 mt-10">  
+    Hello world!
+  </h1>
      
     </>
   )
